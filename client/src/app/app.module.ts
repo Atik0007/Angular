1b59e5f731656fbcee8d78bb71b810676b1eb9c9
PowerCoders <hadraskat@gmail.com>
2022-08-11 11:35:22 +0200

    Reactive-forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { ButtonComponent } from './button/button.component';
 import { FormNewItemComponent } from './form-new-item/form-new-item.component';
 import { CitiesComponent } from './cities/cities.component';
 import { FilterPipe } from './pipes/filter.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactComponent } from './contact/contact.component';
+import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +18,9 @@ import { ContactComponent } from './contact/contact.component';
     CitiesComponent,
     FilterPipe,
     ContactComponent,
+    ContactReactiveComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
