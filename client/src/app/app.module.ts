1b8a63a00fbd50e11e494815d92c6e1725ef2d9c
unknown <hadraskat@gmail.com>
2022-08-16 11:48:39 +0200

    Lazy loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,8 @@ import { ButtonComponent } from './button/button.component';
 import { FormNewItemComponent } from './form-new-item/form-new-item.component';
 import { CitiesComponent } from './cities/cities.component';
 import { FilterPipe } from './pipes/filter.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ContactComponent } from './contact/contact.component';
-import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app.routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -25,7 +24,6 @@ import { ListComponent } from './users/list/list.component';
     CitiesComponent,
     FilterPipe,
     ContactComponent,
-    ContactReactiveComponent,
     HomeComponent,
     NavbarComponent,
     PageNotFoundComponent,
@@ -33,7 +31,7 @@ import { ListComponent } from './users/list/list.component';
     DetailsComponent,
     ListComponent,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
